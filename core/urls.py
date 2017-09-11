7d5ef2b52a88628bcc1fe70daace5f47b490b720
MingLee02 <ming.lee@hotmail.com>
2017-09-11 09:43:23 +0100

    Add initial form to capture reference number.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import MainView
+from .views import MainView, reference
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', MainView.as_view(), name='main'),
+    url(r'^reference/$', reference, name='post_ref'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class MainView(TemplateView):
     template_name = 'core/main.html'
+
+def reference(request):
+   print(int(request._post['ref']))
