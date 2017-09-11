6c4a7fc1cddd1593c143342d424f0ce086354256
MingLee02 <ming.lee@hotmail.com>
2017-09-11 16:44:50 +0100

    Add Export data url.

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from .views import MainView, reference
+from .views import MainView, reference, export
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', MainView.as_view(), name='main'),
     url(r'^reference/$', reference, name='post_ref'),
+    url(r'^export/$', export, name='export'),
 ]
